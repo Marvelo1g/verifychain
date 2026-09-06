e9a1d5728f2caee16d7499bb73a2dd2ad3a4be8d
Marvel <dajiowolabi6@gmail.com>
2026-09-06 16:40:34 -0700

    Deploy VerifyChain to Sepolia testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,12 +2,20 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 import hardhatNodeTestRunner from "@nomicfoundation/hardhat-node-test-runner";
 import hardhatToolboxViem from "@nomicfoundation/hardhat-toolbox-viem";
 import { defineConfig } from "hardhat/config";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export default defineConfig({
   solidity: "0.8.28",
   networks: {
     hardhat: {
       type: "edr-simulated",
+    },
+    sepolia: {
+      type: "http",
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
   plugins: [hardhatToolboxViem, hardhatNodeTestRunner],
